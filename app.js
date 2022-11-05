4199703a152b2e9511367dd89e29f9da78b1412f
Ana Caicedo <anaccaicedoc@gmail.com>
2022-11-04 19:41:37 -0500

    Create first get function

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", function (req, res) {
+  Article.find({}, function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    }
+    res.send(err);
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
